5ba574b9253c56776f6bf22293cbdab660833f9d
tahaamin2013 <tahaamindob2013@gmail.com>
2023-11-28 22:57:55 +0500

    fsda

--- src/components/visa-type-section/VisaType.tsx
+++ src/components/visa-type-section/VisaType.tsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const VisaType = () => {
   return (
-    <div className='flex justify-center flex-col text-center items-center'>
-      <h1 className='text-2xl font-medium'>Select a visa type</h1>
-      <p className=' text-lg text-gray-600'>No hidden fees! All taxes included.</p>
-      <div className="bg-white p-2 py-4 shadow-md w-full mr-48 flex justify-between mt-4">
-        <div>Left Content</div>
+    <div className="flex justify-center flex-col text-center items-center">
+      <h1 className="text-2xl font-medium">Select a visa type</h1>
+      <p className="text-lg text-gray-600">
+        No hidden fees! All taxes included.
+      </p>
+      <div className="p-2 py-4 shadow-md w-96 flex justify-between mt-4 sm:flex-row flex-col">
+      <div className="flex">
+          <div>
+            <input type="radio" name="entry" value="fs" required />
+            Single Entry
+          </div>
+          <div>
+            <input type="radio" name="entry" value="fs" required />
+            Multiple Entry
+          </div>
+        </div>
         <div>Right Content</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default VisaType
+export default VisaType;
