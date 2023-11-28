632ead07e4c8982eb72bf6d7f0acdd64d7f7e0ed
tahaamin2013 <tahaamindob2013@gmail.com>
2023-11-28 14:09:48 +0500

    fdsa

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,7 @@
 
 import React, { useState } from "react";
 import Image from "next/image";
-import { IoClose } from "react-icons/io5";
-import { CiMenuBurger } from "react-icons/ci";
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 import { FaFacebookF, FaTwitter } from "react-icons/fa";
 import Button from "./Button/Button";
 import Link from "next/link";
@@ -12,7 +11,7 @@ const Navbar = () => {
   const links = [
     { name: "APPLY", link: "/" },
     { name: "TRACK", link: "/" },
-    { name: "SERVICES", link: "/" },
+    { name: "FAQ", link: "/" },
     { name: "BLOG", link: "/" },
     { name: "CONTACT", link: "/" },
   ];
@@ -25,32 +24,36 @@ const Navbar = () => {
 
   return (
     <div className="shadow-md w-full top-0 left-0 text-white">
-      <div className="md:flex items-center justify-between bg-[#464646] py-3 w-full h-24 md:h-fit md:px-10 px-11">
+      <div className="md:flex items-center justify-between bg-[#464646] py-3 w-full h-24 md:h-fit md:px-[140px] px-11">
         <div className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] lg:text-gray-800">
           {/* <Image className=" absolute top-2" src="/Logo.png" alt="Logo" width={220} height={20} /> */}
           <Image
             src="/Logo.png"
-            className=" absolute shadow-lg top-0"
+            className=" absolute shadow-lg top-0 "
             alt="Logo"
             width={150}
             height={40}
           />
         </div>
 
-        <div
-          onClick={toggleMenu}
-          className="text-3xl z-30 absolute right-8 top-6 cursor-pointer md:hidden"
-        >
-          <icon>
-            {open ? (
-              <IoClose className="text-white" />
-            ) : (
-              <CiMenuBurger
-                className="text-white"
-              />
-            )}
-          </icon>
+ 
+          <div className="relative">
+      <div
+        onClick={toggleMenu}
+        className="text-3xl z-30 absolute right-8 top-4 cursor-pointer md:hidden transition-all duration-300"
+      >
+                <span className="text-3xl text-white">
+          {open ? (
+            <AiOutlineClose style={{ fontSize: '40px' }} />
+          ) : (
+            <AiOutlineMenu style={{ fontSize: '40px' }} />
+          )}
+        </span> 
+
+      </div>
         </div>
+
+       
 
         <ul
           className={`md:flex md:items-center text-sm md:pb-0 md:mt-0 -mt-3 md:static md:bg-transparent bg-[#464646] z-10 inset-x-0 md:z-auto left-0 w-full h-full md:w-auto md:pl-0 fixed pl-9 transition-all duration-500 ease-in ${
@@ -60,7 +63,7 @@ const Navbar = () => {
           {links.map((link) => (
             <li
               key={link.name}
-              className="md:ml-8 text-xl md:my-0 my-7"
+              className="md:ml-8 text-md md:my-0 my-7"
             >
               <a
                 href={link.link}
@@ -70,22 +73,22 @@ const Navbar = () => {
               </a>
             </li>
           ))}
-          <li className="md:ml-8 text-md md:my-0 my-7 hidden md:block">
-            <Button>EMERGENCY VISA IN 12 HOURS</Button>
+          <li className="md:ml-8 text-sm hidden md:block">
+            <Button>EMERGENCY VISA <br /> IN 12 HOURS</Button>
           </li>
 
           <div className="flex flex-row text-2xl gap-6 lg:mx-4 mx-5 my-4">
             <Link href="/" className="socialLink">
-              <FaFacebookF />
+              <FaFacebookF size={16} />
             </Link>
             <Link href="/" className="socialLink">
-              <FaTwitter />
+              <FaTwitter size={16} />
             </Link>
           </div>
 
-          <select className="bg-transparent">
-            <option value="">English</option>
-            <option value="">العربية</option>
+          <select className="bg-transparent outline-none w-[80px]">
+            <option value="" className="text-black">English</option>
+            <option value="" className="text-black">العربية</option>
           </select>
         </ul>
       </div>
