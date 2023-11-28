ab56a6c79518aac81672a89f3653a7dfcbf64f42
tahaamin2013 <tahaamindob2013@gmail.com>
2023-11-28 16:59:30 +0500

    any for button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Button = (props) => {
+const Button = (props:any) => {
   return (
     <button className='bg-[#e90000] px-3 w-[10rem] pb-2 pt-2 font-medium leading-none rounded-full border-white border-[1px]'>
       {props.children}
