a58e4f70cef1244d7552f027bde1220e5b091c0e
tahaamin2013 <tahaamindob2013@gmail.com>
2023-11-29 17:42:45 +0500

    FDSA

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,21 +1,19 @@
-'use client'
+"use client";
 
 import React from "react";
 import { FaCheck } from "react-icons/fa6";
 
-const scrollToTop =  () => {
+const scrollToTop = () => {
   window.scrollTo({
     top: 400,
     behavior: "smooth", // Optional: Use smooth scrolling
   });
-}
-
+};
 
 const Footer = () => {
-
   return (
     <div className=" bg-white w-full text-center pt-20">
-      <div className="lg:text-start flex flex-col xl:flex-row justify-between sm:text-center lg:mx-20 mx-0">
+      <div className="lg:text-start mb-24 flex flex-col xl:flex-row justify-between sm:text-center lg:mx-20 mx-0">
         <div>
           <h1 className="text-xl flex text-center justify-center items-center lg:text-2xl font-medium">
             Want a Visa by Thursday, 30 Nov 2023?
@@ -37,10 +35,33 @@ const Footer = () => {
         </button>
       </div>
       <hr />
-      <div className=" p-48"></div>
+      <div>
+        <div>
+          <h1>Get in touch </h1>
+          <a href="">support@emiratesvisa.com</a>
+          <a href="">WhatsApp</a>
+          <a href="">+971 50 803 8472</a>
+          <a href="">Down Town Dubai</a>
+        </div>
+        <div>
+          <h1>Help & Support</h1>
+          <a href="">support@emiratesvisa.com</a>
+          <a href="">FAQ</a>
+        </div>
+        <div>
+          <h1>Hangout with Us</h1>
+          <p>Facebook</p>
+        </div>
+        <div>
+          <h1>About us</h1>
+          <a href="">About Us</a>
+          <a href="">Term of use</a>
+          <a href="">Privacy Policy</a>
+          <a href="">Careers</a>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Footer;
-  
