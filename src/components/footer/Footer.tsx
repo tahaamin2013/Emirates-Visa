b48626a2898a64ce95e6c2eee55822642d722bd0
tahaamin2013 <tahaamindob2013@gmail.com>
2023-11-29 18:35:40 +0500

    commit

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { FaCheck } from "react-icons/fa6";
+import Link from "next/link";
 
 const scrollToTop = () => {
   window.scrollTo({
@@ -12,13 +13,13 @@ const scrollToTop = () => {
 
 const Footer = () => {
   return (
-    <div className=" bg-white w-full text-center pt-20">
+    <div className=" bg-white w-full pt-20">
       <div className="lg:text-start mb-24 flex flex-col xl:flex-row justify-between sm:text-center lg:mx-20 mx-0">
         <div>
           <h1 className="text-xl flex text-center justify-center items-center lg:text-2xl font-medium">
             Want a Visa by Thursday, 30 Nov 2023?
           </h1>
-          <p className="text-[#ef1810] text-xl font-normal lg:font-medium">
+          <p className="text-[#ef1810] xl:ml-0 ml-12 text-xl font-normal lg:font-medium">
             Apply within 3 Hrs 58 Mins
           </p>
         </div>
@@ -35,31 +36,89 @@ const Footer = () => {
         </button>
       </div>
       <hr />
-      <div>
-        <div>
-          <h1>Get in touch </h1>
-          <a href="">support@emiratesvisa.com</a>
-          <a href="">WhatsApp</a>
-          <a href="">+971 50 803 8472</a>
-          <a href="">Down Town Dubai</a>
+
+      <footer className="bg-white dark:bg-gray-900">
+        <div className="ml-[70px] w-full max-w-screen-xl">
+          <div className="grid grid-cols-1 text-left gap-8 px-4 py-6 lg:py-8 md:text-left md:grid-cols-4">
+
+            {/* Get in touch */}
+            <div>
+              <h2 className="mb-6 text-black">Get in touch</h2>
+              <ul className="text-gray-700 font-light">
+                <li className="mb-4">
+                  <Link href="#" className="hover:underline">
+                    support@emiratesvisa.com
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link href="#" className="hover:underline">
+                    WhatsApp
+                  </Link>
+                </li>
+                <li className="mb-4">+971 50 803 8472</li>
+                <li className="mb-4">Down Town Dubai</li>
+              </ul>
+            </div>
+
+            {/* Help & Support */}
+            <div>
+              <h2 className="mb-6 text-black">Help & Support</h2>
+              <ul className="text-gray-700 font-light">
+                <li className="mb-4">
+                  <Link href="#" className="hover:underline">
+                    support@emiratesvisa.com
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link href="#" className="hover:underline">
+                    FAQ
+                  </Link>
+                </li>
+              </ul>
+            </div>
+
+            {/* Hangout with Us */}
+            <div>
+              <h2 className="mb-6 text-black">Hangout with Us</h2>
+              <ul className="text-gray-700 font-light">
+                <li className="mb-4">
+                  <Link href="#" className="hover:underline">
+                    Facebook
+                  </Link>
+                </li>
+              </ul>
+            </div>
+
+            {/* About us */}
+            <div>
+              <h2 className="mb-6 text-black">About us</h2>
+              <ul className="text-gray-700 font-light">
+                <li className="mb-4">
+                  <Link href="/about" className="hover:underline">
+                    About Us
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link href="#" className="hover:underline">
+                    Term of use
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link href="#" className="hover:underline">
+                    Privacy Policy
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link href="#" className="hover:underline">
+                    Careers
+                  </Link>
+                </li>
+              </ul>
+            </div>
+
+          </div>
         </div>
-        <div>
-          <h1>Help & Support</h1>
-          <a href="">support@emiratesvisa.com</a>
-          <a href="">FAQ</a>
-        </div>
-        <div>
-          <h1>Hangout with Us</h1>
-          <p>Facebook</p>
-        </div>
-        <div>
-          <h1>About us</h1>
-          <a href="">About Us</a>
-          <a href="">Term of use</a>
-          <a href="">Privacy Policy</a>
-          <a href="">Careers</a>
-        </div>
-      </div>
+      </footer>
     </div>
   );
 };
