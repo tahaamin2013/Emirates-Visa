14fdb9bf5996f719cf385fdc6bbdd978a17e3030
tahaamin2013 <tahaamindob2013@gmail.com>
2023-11-29 05:11:25 +0500

    Update The Service Section

--- src/components/Serice-Section/Service.tsx
+++ src/components/Serice-Section/Service.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Service = ({title, description}: {title: string, description: string}) => {
   return (
-    <div className="bg-white w-[305px] lg:w-96 text-center p-[35.2px] pt-[32px] pb-[43px]">
-          <p className="text-md text-gray-600">
+    <div className="bg-white w-[339px] h-[250px] lg:h-[215px] text-center align-middle flex flex-col items-center p-[35.2px] pt-[32px] pb-[43px]">
+          <p className="text-md text-gray-600 w-44">
             {title}
           </p>
-          <hr className=" border-b-[#e90000] border-[3.4px] border-transparent ml-[70px] lg:ml-[88px] w-[150px] mt-3 rounded-full mb-3" />
-          <p>
+          <hr className=" border-b-[#e90000] border-[3.4px] border-transparent w-[150px] mt-3 rounded-full mb-3" />
+          <p className='text-gray-700 text-sm'>
             {description}
           </p>
         </div>
--- src/components/Serice-Section/ServicesSection.tsx
+++ src/components/Serice-Section/ServicesSection.tsx
@@ -10,13 +10,21 @@ const ServicesSection = () => {
       <p className=" text-sm lg:text-lg text-gray-600">
         Application in minutes - 24/7 help & support
       </p>
-      <hr className=" border-b-[#e90000] border-transparent border-2 w-[200px] mt-6 rounded-full mb-28" />
-      <div className=" flex flex-col items-center mt-[-30px] gap-3 lg:flex-row">
-        <Service title="Apply online anywhere anytime" description="We make it easy and secure to apply online. We take your privacy seriously, data is encrypted with industry-leading 256-bit encryption." />
-        <Service title="Get out-standing 24x7 customer service" description="Our team is available for you to get you visa hassle free. You choose from 1 of 6 communication channel of your choice." />
-        <Service title="Enjoy 24 hours express visa service" description="Enjoy 24 hours express service as standard without paying extra. No hidden charges with us and you get full tracking." />
+      <hr className=" border-b-[rgb(233,0,0)] border-transparent border-2 w-[200px] mt-6 rounded-full mb-28" />
+      <div className="flex flex-col items-center mt-[-30px] gap-3 lg:flex-row">
+        <Service
+          title="Apply online anywhere anytime"
+          description="We make it easy and secure to apply online. We take your privacy seriously, data is encrypted with industry-leading 256-bit encryption."
+        />
+        <Service
+          title="Get out-standing 24x7 customer service"
+          description="Our team is available for you to get you visa hassle free. You choose from 1 of 6 communication channel of your choice."
+        />
+        <Service
+          title="Enjoy 24 hours express visa service"
+          description="Enjoy 24 hours express service as standard without paying extra. No hidden charges with us and you get full tracking."
+        />
       </div>
-      <div className=" my-44"></div>
     </div>
   );
 };
