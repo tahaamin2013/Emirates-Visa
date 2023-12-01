d73d8d12273c264679a19a2837f1aaf264eae68b
tahaamin2013 <tahaamindob2013@gmail.com>
2023-12-01 21:05:51 +0500

    time

--- src/components/footer/Foo.tsx
+++ src/components/footer/Foo.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image";
-import React from "react";
-import { CiClock2 } from "react-icons/ci";
+import React, { useState, useEffect } from "react";
 import { FaCheck } from "react-icons/fa";
-import LiveTime from "../LiveTime";
+import LiveTime from "../LiveTime"; // Import the LiveTime component
+import moment from "moment-timezone"; // Import moment-timezone
 
 const scrollToTop = () => {
   window.scrollTo({
@@ -12,23 +11,30 @@ const scrollToTop = () => {
 };
 
 const Foo = () => {
+  const [dubaiTime, setDubaiTime] = useState(moment.tz("Asia/Dubai").format("hh:mm A")); // Set initial Dubai time
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDubaiTime(moment.tz("Asia/Dubai").format("hh:mm A")); // Update Dubai time every second
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
-    <LiveTime />
-    <div className="w-full h-fit bg-white-500 mb-4 border-b px-18 sm:px-28 py-24 flex flex-col text-center sm:text-left sm:flex-row items-center justify-between gap-6">
-      <div>
-        <h1 className="sm:text-2xl text-xl">Want a Visa by Sunday, 3 Dec 2023?</h1>
-        <p className="text-xl text-red-500">Apply within 3 Hrs 58 Mins</p>
-      </div>
-
+      <LiveTime currentTime={dubaiTime} /> {/* Display live time component */}
+      <div className="w-full h-fit bg-white-500 mb-4 border-b px-18 sm:px-28 py-24 flex flex-col text-center sm:text-left sm:flex-row items-center justify-between gap-6">
+        <div>
+          <h1 className="sm:text-2xl text-xl">Want a Visa by Sunday, 3 Dec 2023?</h1>
+          <p className="text-xl" style={{ color: "#e90000" }}>Apply within 3 Hrs 58 Mins</p>
+        </div>
         <button className="flex w-[210px] h-[50px] items-center rounded-md justify-center border-white bg-[#e90000] font-[400] capitalize text-white" onClick={scrollToTop}>
           <p>APPLY NOW</p>
           <FaCheck className="mt-0 ml-4 text-2xl" />
         </button>
-    </div>
+      </div>
     </>
-
-   
   );
 };
 
