e4823aa2fd3b5777b693e4eb74b674d9b85ce020
tahaamin2013 <tahaamindob2013@gmail.com>
2023-12-01 20:51:11 +0500

    d

--- src/components/Contry-Selection/Country-Selection.tsx
+++ src/components/Contry-Selection/Country-Selection.tsx
@@ -186,7 +186,7 @@ const CountrySelection = () => {
             <button /* Search Button 182 */
               type="button"
               disabled={isButtonDisabled} // Add this line
-              class={`bg-white shadow-md transition-all ease-in hover:bg-gray-100 text-red-600 ${styles.button}`}
+              className={`bg-white shadow-md transition-all ease-in hover:bg-gray-100 text-red-600 ${styles.button}`}
               onClick={SearchtheVisa}
             >
               <IoSearchSharp size={40} />
@@ -194,7 +194,7 @@ const CountrySelection = () => {
           ) : (
             <button /* Search Button 345 */
               type="button"
-              class={`bg-white transition-all ease-in hover:bg-gray-100 shadow-md text-red-600 ${styles.button}`}
+              className={`bg-white transition-all ease-in hover:bg-gray-100 shadow-md text-red-600 ${styles.button}`}
               onClick={ResettheVisa}
 
             >
