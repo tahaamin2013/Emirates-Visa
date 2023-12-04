dc4d87eab5e3b7458eb6cd4b5398917395a7e4a1
tahaamin2013 <tahaamindob2013@gmail.com>
2023-12-04 23:30:00 +0500

    Remove bg color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 import ServicesSection from "@/components/Service-Section/ServicesSection";
 import dynamic from "next/dynamic";
 
-const HeroSection = dynamic(()=>import('../components/heroSection/HeroSection'))
+const HeroSection = dynamic(
+  () => import("../components/heroSection/HeroSection")
+);
 
 export default function Home() {
   return (
     <>
-    <div className=" bg-emerald-500">
-      <HeroSection />
-      <ServicesSection />
-      
+      <div className="m-0">
+        <HeroSection />
+        <ServicesSection />
       </div>
-      </>
-  )
+    </>
+  );
 }
--- src/components/Service-Section/Service.tsx
+++ src/components/Service-Section/Service.tsx
@@ -14,7 +14,6 @@ const Service = ({
     <div
       className={`bg-white w-[290px] h-[220px] lg:h-[220px] text-center align-middle flex flex-col items-center p-[35.2px] pt-[32px] pb-[43px] ${className}`}
     >
-
       <p className="text-md text-gray-600 w-44">{title}</p>
       <hr className=" border-b-[#e90000] border-[3.4px] border-transparent w-[150px] mt-3 rounded-full mb-3" />
       <p className="text-gray-700 text-sm">{description}</p>
