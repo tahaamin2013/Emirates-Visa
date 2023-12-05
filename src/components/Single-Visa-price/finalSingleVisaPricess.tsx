3b800f2277a689aa6895e70829ca35c744192364
tahaamin2013 <tahaamindob2013@gmail.com>
2023-12-06 02:39:03 +0500

    Next try

--- src/components/Single-Visa-price/finalSingleVisaPricess.tsx
+++ src/components/Single-Visa-price/finalSingleVisaPricess.tsx
@@ -13,8 +13,11 @@ import {
 import Image from "next/image";
 import React, { ChangeEvent, useEffect, useState } from "react";
 import { FaCheck } from "react-icons/fa6";
+import { AiOutlineDelete } from "react-icons/ai"; // Import the delete icon
 import { IoIosArrowForward } from "react-icons/io";
 import Steps from "../Terms-and-condition/Steps";
+import { Download } from "lucide-react";
+import Fileuploader from "./Fileuploader";
 
 type Country = {
   name: {
@@ -50,24 +53,24 @@ const FinalSingleVisaPricess = ({
   const [uploadProgress, setUploadProgress] = useState(0); // Track upload progress
   const [selectedFile, setSelectedFile] = useState(null);
 
-  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files[0];
-    if (file) {
-      setIsLoading(true);
-      setSelectedFile(file); // Set the selected file
+  // const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+  //   const file = event.target.files[0];
+  //   if (file) {
+  //     setIsLoading(true);
+  //     setSelectedFile(file); // Set the selected file
 
-      const interval = setInterval(() => {
-        setUploadProgress((prevProgress) => {
-          if (prevProgress < 100) {
-            return prevProgress + 10;
-          }
-          clearInterval(interval);
-          setIsLoading(false);
-          return prevProgress;
-        });
-      }, 500);
-    }
-  };
+  //     const interval = setInterval(() => {
+  //       setUploadProgress((prevProgress) => {
+  //         if (prevProgress < 100) {
+  //           return prevProgress + 10;
+  //         }
+  //         clearInterval(interval);
+  //         setIsLoading(false);
+  //         return prevProgress;
+  //       });
+  //     }, 500);
+  //   }
+  // };
 
   const handleDialogToggle2 = () => {
     setIsChecked1(false);
@@ -403,7 +406,9 @@ const FinalSingleVisaPricess = ({
                             </div>
 
                             <div className="flex flex-col xl:flex-row justify-center gap-5 mt-6">
-                              <div>
+                              <Fileuploader />
+                              {/* <div className="bg-white border hover:shadow-xl transition-all p-5 flex items-center flex-col">
+                                Passport
                                 <label htmlFor="file-upload">
                                   <div className="custom-file-upload relative">
                                     <input
@@ -416,15 +421,26 @@ const FinalSingleVisaPricess = ({
                                     />
                                     {isLoading ? (
                                       <div className="status">
-                                        <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-    </svg>
+                                        <svg
+                                          aria-hidden="true"
+                                          className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                                          viewBox="0 0 100 101"
+                                          fill="none"
+                                          xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                          <path
+                                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                            fill="currentColor"
+                                          />
+                                          <path
+                                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                            fill="currentFill"
+                                          />
+                                        </svg>
                                       </div>
                                     ) : (
-                                      <div className="flex items-center justify-center">
-                                        <i className="fas fa-image"></i>
-                                        <span>Select Image</span>
+                                      <div className="flex text-red-500 items-center justify-center">
+                                        <Download />
                                       </div>
                                     )}
                                   </div>
@@ -432,7 +448,7 @@ const FinalSingleVisaPricess = ({
                                 {selectedFile && !isLoading && (
                                   <p>Selected file: {selectedFile.name}</p>
                                 )}
-                              </div>
+                              </div> */}
                             </div>
                           </div>
                         </div>
